17dc7a165622ee06823420cfda6586d78c8518e9
szGeorgiy <166986665+szGeorgiy@users.noreply.github.com>
2024-08-19 16:10:18 +0300

    Delete HW_9 directory

--- HW_9/task13_number_from_to.c
+++
@@ -1,9 +0,0 @@
-int count_between(int from, int to, int size, int a[]){
-    int ret = 0;
-    for (int i = 0; i < size; i++){
-        if (a[i] >= from && a[i] <= to){
-            ret++;
-        }
-    }
-    return ret;
-}
